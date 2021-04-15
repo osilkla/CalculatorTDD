20cee1f74d0200dcc4f54243d248fd87d92ed6ca
fargeaudou <sacha.fargeaudou@io-lab.fr>
2021-04-15 16:36:06 +0200

    green: fix divide several nbrs

--- src/divide/divide.js
+++ src/divide/divide.js
@@ -1,7 +1,7 @@
 function divide(...numb){
   if(numb.length==1) return numb[0]
   if(numb.findIndex((num)=>num === 0) != -1) throw new Error("can't divide by 0")
-  return numb.reduce((total,num)=>total/num,1)
+  return numb.reduce((total,num)=>total/num)
 }
 
 module.exports = divide;
