f3364edadb8557b4cbc693de0b4488ec9748637b
fargeaudou <sacha.fargeaudou@io-lab.fr>
2021-04-15 15:46:26 +0200

    red: not able to add more than 2 number

--- adder.test.js
+++ adder.test.js
@@ -7,4 +7,6 @@ test('adds 1 + 2 to equal 3', () => {
 test('should return the number if one arg is passed', () => {
   expect(sum(1)).toBe(1);
 });
- 
+test('should be able to add 3 numbers', () => {
+  expect(sum(1,2,3)).toBe(6);
+});
