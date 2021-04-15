096faf209eef03775e02df024b97cf2345ad8542
fargeaudou <sacha.fargeaudou@io-lab.fr>
2021-04-15 15:44:39 +0200

    red: add function need two args error

--- adder.test.js
+++ adder.test.js
@@ -4,3 +4,7 @@ const { expect } = require("@jest/globals")
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+test('should return the number if one arg is passed', () => {
+  expect(sum(1)).toBe(1);
+});
+ 
